64f7bed11ea8c5c653d5e14f075f118ff6062658
waad <waadmahmoud2044@gmail.com>
2024-08-04 17:36:48 +0200

    app created

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:todo_app/HomeScreen.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName:(context)=>HomeScreen(),
+      },
+    );
+  }
+}
